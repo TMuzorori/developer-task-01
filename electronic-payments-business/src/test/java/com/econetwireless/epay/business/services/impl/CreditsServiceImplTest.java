f731fb87a045db0b8b79775f76919d4d4fdcb33f
timothy <timothy.mozorori@sov.tech>
2021-03-21 23:16:06 +0200

    Test: test directory abd TestUtils class

---
+++ electronic-payments-business/src/test/java/com/econetwireless/epay/business/services/impl/CreditsServiceImplTest.java
@@ -0,0 +1,55 @@
+package com.econetwireless.epay.business.services.impl;
+
+import com.econetwireless.epay.business.integrations.api.ChargingPlatform;
+import com.econetwireless.epay.business.services.api.CreditsService;
+import com.econetwireless.epay.business.utils.TestUtils;
+import com.econetwireless.epay.dao.subscriberrequest.api.SubscriberRequestDao;
+import com.econetwireless.epay.domain.SubscriberRequest;
+import com.econetwireless.utils.messages.AirtimeTopupRequest;
+import com.econetwireless.utils.messages.AirtimeTopupResponse;
+import com.econetwireless.utils.pojo.INCreditRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static junit.framework.TestCase.assertNull;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+public class CreditsServiceImplTest {
+    @Mock
+    private SubscriberRequestDao requestDao;
+    @Mock
+    private ChargingPlatform platform;
+
+    private CreditsService creditsService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        when(requestDao.save(any(SubscriberRequest.class))).then(TestUtils.SUBSCRIBER_REQUEST_ANSWER);
+        when(platform.enquireBalance(anyString(), anyString())).then(TestUtils.SUCCESSFUL_BALANCE_ENQUIRY);
+        when(platform.creditSubscriberAccount(any(INCreditRequest.class))).then(TestUtils.SUCCESSFUL_CREDIT_REQUEST);
+        this.creditsService = new CreditsServiceImpl(platform, requestDao);
+    }
+
+
+    @Test
+    public void testNoneNullTopUpRequest() {
+        AirtimeTopupResponse topupResponse = creditsService.credit(new AirtimeTopupRequest());
+        assertNotNull(topupResponse);
+        assertNull(topupResponse.getMsisdn());
+    }
+
+    @Test
+    public void testNoneNullTopUpRequestWithMsiSdnNull() {
+        AirtimeTopupRequest request = new AirtimeTopupRequest();
+        request.setMsisdn("status");
+        AirtimeTopupResponse topupResponse = creditsService.credit(request);
+        assertNotNull(topupResponse);
+    }
+
+}
---
+++ electronic-payments-business/src/test/java/com/econetwireless/epay/business/services/impl/PartnerCodeValidatorImplTest.java
@@ -0,0 +1,47 @@
+package com.econetwireless.epay.business.services.impl;
+
+import com.econetwireless.epay.business.services.api.PartnerCodeValidator;
+import com.econetwireless.epay.business.utils.TestUtils;
+import com.econetwireless.epay.dao.requestpartner.api.RequestPartnerDao;
+import com.econetwireless.utils.execeptions.EpayException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+public class PartnerCodeValidatorImplTest {
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+    @Mock
+    private RequestPartnerDao requestPartnerDao;
+
+    private PartnerCodeValidator partnerCodeValidator;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        when(requestPartnerDao.findByCode(anyString())).then(TestUtils.REQUEST_PARTNER_ANSWER);
+        partnerCodeValidator = new PartnerCodeValidatorImpl(requestPartnerDao);
+    }
+
+    @Test
+    public void testNullPartnerCode() {
+        exception.expect(EpayException.class);
+        partnerCodeValidator.validatePartnerCode(null);
+    }
+
+    @Test
+    public void testNoneNullPartnerCode() {
+
+        boolean validatePartnerCode = partnerCodeValidator.validatePartnerCode("Test");
+        assertTrue(validatePartnerCode);
+
+    }
+
+}
